ec5b27beab6a94cc1b7fcd504d93e3723fb95f47
huangjin <519730485@qq.com>
2017-11-03 17:07:19 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  extends: 'vue',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {}
+}
